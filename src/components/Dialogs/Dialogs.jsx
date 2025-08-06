05a004cdc2e30fa1b735250a2fbf05d45dd09f3c
Slav Mosoyan <vladanik1999@gmail.com>
2025-08-06 15:58:01 +0400

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 export default function Dialogs() {
   return (
     <div className={s.dialogs}>
       <div className={s.items}>
-        <div className={s.item}>Veo</div>
-        <div className={s.item}>Neo</div>
-        <div className={s.item}>Leo</div>
-        <div className={s.item}>Deo</div>
-        <div className={s.item}>Seo</div>
+        <div className={s.item}>
+          <NavLink to="/dialogs/1">Veo </NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink to="/dialogs/2">Veo </NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink to="/dialogs/3">Veo </NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink to="/dialogs/4">Veo </NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink to="/dialogs/5">Veo </NavLink>
+        </div>
       </div>
       <div className={s.messages}>
         <div className={s.dialog}>hello</div>
