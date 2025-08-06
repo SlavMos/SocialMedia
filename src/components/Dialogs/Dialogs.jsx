404382372c32c5d967952ec6fd56cc61fc76124e
Slav Mosoyan <vladanik1999@gmail.com>
2025-08-06 15:26:07 +0400

    fx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 import s from "./Dialogs.module.css";
 export default function Dialogs() {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.items}>
+        <div className={s.item}>Veo</div>
+        <div className={s.item}>Neo</div>
+        <div className={s.item}>Leo</div>
+        <div className={s.item}>Deo</div>
+        <div className={s.item}>Seo</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.dialog}>hello</div>
+        <div className={s.dialog}>hello</div>
+        <div className={s.dialog}>hello</div>
+        <div className={s.dialog}>hello</div>
+        <div className={s.dialog}>hello</div>
+      </div>
+    </div>
+  );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,25 +4,22 @@ import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div>
-      {" "}
-      <nav className={s.nav}>
-        <div>
-          <NavLink to="/profile">Profile</NavLink>
-        </div>
-        <div>
-          <NavLink to="/dialogs">Dialogs</NavLink>
-        </div>
-        <div>
-          <NavLink to="/dialogs">Dialogs</NavLink>
-        </div>
-        <div>
-          <NavLink to="/dialogs">Dialogs</NavLink>
-        </div>
-        <div>
-          <NavLink to="/dialogs">Dialogs</NavLink>
-        </div>
-      </nav>
-    </div>
+    <nav className={s.nav}>
+      <div>
+        <NavLink to="/profile">Profile</NavLink>
+      </div>
+      <div>
+        <NavLink to="/dialogs">Dialogs</NavLink>
+      </div>
+      <div>
+        <NavLink to="/dialogs">Dialogs</NavLink>
+      </div>
+      <div>
+        <NavLink to="/dialogs">Dialogs</NavLink>
+      </div>
+      <div>
+        <NavLink to="/dialogs">Dialogs</NavLink>
+      </div>
+    </nav>
   );
 }
