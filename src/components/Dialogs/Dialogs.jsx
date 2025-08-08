aea54306b97332e638c0e326d84877e2f43b6a45
Slav Mosoyan <vladanik1999@gmail.com>
2025-08-08 17:15:22 +0400

    map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,32 +1,41 @@
 import React from "react";
 import s from "./Dialogs.module.css";
 import { NavLink } from "react-router-dom";
+
 export default function Dialogs() {
+  let dialogsData = [
+    { id: 1, name: "Bla" },
+    { id: 2, name: "Bla" },
+    { id: 3, name: "Bla" },
+    { id: 4, name: "Blass" },
+    { id: 5, name: "Bla" },
+  ];
+
+  let messagesData = [
+    { id: 1, text: "hello" },
+    { id: 2, text: "hello" },
+    { id: 3, text: "hello" },
+    { id: 4, text: "hello" },
+    { id: 5, text: "hello moto" },
+  ];
+  <NavLink to="/dialogs/1">Veo </NavLink>;
   return (
     <div className={s.dialogs}>
       <div className={s.items}>
         <div className={s.item}>
-          <NavLink to="/dialogs/1">Veo </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/2">Veo </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/3">Veo </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/4">Veo </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/5">Veo </NavLink>
+          {dialogsData.map((dialogs) => (
+            <div className={s.item} key={dialogs.id}>
+              <NavLink to={`/dialogs/${dialogs.id}`}>{dialogs.name}</NavLink>
+            </div>
+          ))}
         </div>
       </div>
       <div className={s.messages}>
-        <div className={s.dialog}>hello</div>
-        <div className={s.dialog}>hello</div>
-        <div className={s.dialog}>hello</div>
-        <div className={s.dialog}>hello</div>
-        <div className={s.dialog}>hello</div>
+        {messagesData.map((messages) => (
+          <div className={s.dialog} key={messages.id}>
+            {messages.text}
+          </div>
+        ))}
       </div>
     </div>
   );
