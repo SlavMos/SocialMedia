1f127da1814a6ca29387e7c3d0bbe87e4d090327
Slav Mosoyan <vladanik1999@gmail.com>
2025-09-09 16:44:25 +0400

    upgrtd

--- src/redux/dialog-reducer.js
+++ src/redux/dialog-reducer.js
@@ -1,3 +1,20 @@
 const dialogReducer = (state, action) => {
-  return state;
+  // Добавление поста
+
+  // Обновление текста нового сообщения (печатаем в инпуте сообщений)
+  if (action.type === "UPDATE-NEW-MESSAGE-BODY") {
+    state.newMessageBody = action.body;
+  }
+
+  // Отправка нового сообщения
+  else if (action.type === "SEND_MESSAGE") {
+    let body = state.newMessageBody; // берем текст из инпута
+    if (body.trim() === "") return;
+    state.newMessageBody = ""; // очищаем инпут
+
+    state.messagesData.push({
+      id: state.messagesData.length + 1, // новый id
+      text: body, // сам текст сообщения
+    });
+  }
 };
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,3 +1,18 @@
 const profileReducer = (state, action) => {
+  // Добавление поста
+  if (action.type === "ADD-POST") {
+    let newPost = {
+      id: state.posts.length + 1, // генерим новый id
+      message: action.message, // текст поста
+      likesCount: 0, // у нового поста лайков пока нет
+    };
+
+    state.posts.push(newPost); // добавляем пост
+  }
+
+  // Обновление текста нового поста (обычно когда печатаем в textarea)
+  else if (action.type === "UPDATE-NEW-POST-TEXT") {
+    state.newMessageBody = action.body;
+  }
   return state;
 };
