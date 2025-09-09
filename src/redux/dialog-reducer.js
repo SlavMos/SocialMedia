2c9ea6afd2813fb9f59fe511ca36d66948e51182
Slav Mosoyan <vladanik1999@gmail.com>
2025-09-09 19:22:16 +0400

    idk

--- src/redux/dialog-reducer.js
+++ src/redux/dialog-reducer.js
@@ -1,19 +1,23 @@
 export const dialogReducer = (state, action) => {
-  // Обновление текста нового сообщения (печатаем в инпуте сообщений)
-  if (action.type === "UPDATE-NEW-MESSAGE-BODY") {
-    state.newMessageBody = action.body;
-  }
+  switch (action.type) {
+    case "UPDATE-NEW-MESSAGE-BODY":
+      {
+        state.newMessageBody = action.body;
+      }
+      return state;
+    case "SEND_MESSAGE":
+      {
+        let body = state.newMessageBody; // берем текст из инпута
+        if (body.trim() === "") return;
+        state.newMessageBody = ""; // очищаем инпут
 
-  // Отправка нового сообщения
-  else if (action.type === "SEND_MESSAGE") {
-    let body = state.newMessageBody; // берем текст из инпута
-    if (body.trim() === "") return;
-    state.newMessageBody = ""; // очищаем инпут
-
-    state.messagesData.push({
-      id: state.messagesData.length + 1, // новый id
-      text: body, // сам текст сообщения
-    });
+        state.messagesData.push({
+          id: state.messagesData.length + 1, // новый id
+          text: body, // сам текст сообщения
+        });
+      }
+      return state;
+    default:
+      return state;
   }
-  return state;
 };
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,18 +1,21 @@
 export const profileReducer = (state, action) => {
-  // Добавление поста
-  if (action.type === "ADD-POST") {
-    let newPost = {
-      id: state.posts.length + 1, // генерим новый id
-      message: action.message, // текст поста
-      likesCount: 0, // у нового поста лайков пока нет
-    };
+  switch (action.type) {
+    case "ADD-POST":
+      let newPost = {
+        id: state.posts.length + 1, // генерим новый id
+        message: action.message, // текст поста
+        likesCount: 0, // у нового поста лайков пока нет
+      };
 
-    state.posts.push(newPost); // добавляем пост
+      state.posts.push(newPost); // добавляем пост
+      state.newPostText = "";
+      return state;
+    case "UPDATE-NEW-POST-TEXT":
+      {
+        state.newPostText = action.message;
+      }
+      return state;
+    default:
+      return state;
   }
-
-  // Обновление текста нового поста (обычно когда печатаем в textarea)
-  else if (action.type === "UPDATE-NEW-POST-TEXT") {
-    state.newMessageBody = action.body;
-  }
-  return state;
 };
